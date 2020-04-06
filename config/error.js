95ca88726b2b0963e803e640e6522ab36d65c00c
Bruno Moura <bruno8moura@gmail.com>
2020-04-06 06:45:26 -0300

    Refactor error handler.

--- config/error.js
+++ config/error.js
@@ -1,13 +1,29 @@
+var createError = require('http-errors');
 const logger = require('../logger').logger();
 const Message = require('../core/error/Message');
 
 module.exports = (app) => {
-    app.use((err, req, res, next) => {
-        logger.error(err);
-        
-        let msg = new Message('Bad things happened, sorry for that. Please contact administrator.', req.id, null);
-        
-        res.status(500).send(msg);
-        res.end();
-      });
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  app.use((err, req, res, next) => {
+    logger.error(err);
+
+    if(err.status === 404 ){
+      res.status(err.status).send();
+      res.end();
+      
+      return;
+    }
+    
+    if(err.status === 500 ){
+      let msg = new Message('Bad things happened, sorry for that. Please contact administrator.', req.id, null);
+      res.status(err.status).send(msg);
+      res.end();
+
+      return;
+    }
+
+  });
 };
